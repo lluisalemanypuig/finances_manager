fd458d083d29b62c837bf642d7f8c9ce3d37e643
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-06-08 15:13:33 +0200

    Added a license header to all the files

--- src/menu_expenses.rs
+++ src/menu_expenses.rs
@@ -1,3 +1,35 @@
+/*********************************************************************
+ *
+ * Finances Manager -- A command line utility to manage financial domestic
+ * activities.
+ *
+ * Copyright (C) 2024
+ *
+ * This file is part of Finances manager. The full code is available at:
+ *      https://github.com/lluisalemanypuig/finances_manager.git
+ *
+ * Finances Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Finances Manager is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Finances Manager.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact:
+ *
+ *     Lluís Alemany Puig
+ *         email: lluis.alemany.puig@gmail.com
+ *         https://github.com/lluisalemanypuig
+ *         lluisalemanypuig.github.io
+ *
+ ********************************************************************/
+
 extern crate chrono;
 use chrono::prelude::*;
 
